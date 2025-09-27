238987fdb1a37a079762a69981ff8a8205c60fb2
Omar Elshaikh <omarmohammedomarali@gmail.com>
2025-09-27 04:26:49 +0300

    debuging the terminal --verbose flag.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 from dotenv import load_dotenv
@@ -6,10 +7,14 @@ from google.genai import types
 
 
 def main():
-    if len(sys.argv) > 2 :
+    if len(sys.argv) < 2 :
         print("usage: python3 main.py [prompet]")
         exit(1)
-
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
     user_prompet = " ".join(sys.argv[1:])
     Messages = [
         types.Content(role="user",parts=[types.Part(text=user_prompet)])
@@ -19,16 +24,19 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=Messages
+    def req(client,messages,verbose):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", contents=messages
 )
-
-
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
     
-
+#        nonlocal vrep
+        if verbose:
+        
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+            print(f"User prompt: {user_prompt}\n")
+        print(response.text)    
+    req(client,Messages,verbose)
 if __name__ == "__main__":
     main()
