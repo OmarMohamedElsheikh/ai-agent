afbd9252eebc0ae25ad68fc8369e224b3272ccca
OmarMohamedElsheikh <omarmohammedomarali@gmail.com>
2025-12-18 07:13:08 +0200

    implementing verbose output

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ from google.genai import types
 load_dotenv()
 
 
-praser = argparse.ArgumentParser(
+parser = argparse.ArgumentParser(
     prog="gimini-api agent",
     description="ai-agent chatbot",
     
 )
 
-praser.add_argument("user_prompt", type=str , help="user prompt")
-args = praser.parse_args()
+parser.add_argument("user_prompt", type=str , help="user prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
+args = parser.parse_args()
 
 
 messages = [genai.types.Content(role="user",parts=[types.Part(text=args.user_prompt)])]
@@ -35,8 +37,9 @@ content = client.models.generate_content(
 if not content.usage_metadata :
     raise RuntimeError("faild request!")
 
-
-print("Prompt tokens:", content.usage_metadata.prompt_token_count)
-print("Response tokens:", content.usage_metadata.candidates_token_count)
+if args.verbose:
+    print("User prompt: " , args.user_prompt )
+    print("Prompt tokens:", content.usage_metadata.prompt_token_count)
+    print("Response tokens:", content.usage_metadata.candidates_token_count)
 
 print(content.text)
